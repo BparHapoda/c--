52516ad5b40a3b5de99138f5dace344e52e851e8
Denis BparHapoda Boronin <77019249+BparHapoda@users.noreply.github.com>
2022-12-03 17:29:08 +0300

    Update program2.cs

--- program2.cs
+++ program2.cs
@@ -1,1 +1,24 @@
+//Задача 2: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе. Реализовать через функции.
 
+string a = inputInt ("Введите число ");
+Console.WriteLine("Cумма цифр = "+ sumNumbers (a));
+
+
+string sumNumbers (string a) {
+if (a.Length==1){return a;}
+int number = Convert.ToInt32(a);
+int result=0;
+
+    for(int i=0;i<a.Length;i++){
+        result=result+number%10;
+        number=number/10;
+
+    }
+    return Convert.ToString(result);
+}
+
+string inputInt (string n){
+    Console.WriteLine("Введите число "+ n + ":");
+    string a = Console.ReadLine();
+    return a;
+}
