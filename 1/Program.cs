145bdc270c20dbe418d8667800e9836b9d6f6710
Bpar_Hapoda <cddvdrom@yandex.ru>
2022-12-06 02:36:17 +0300

    last 1 1

--- 1/Program.cs
+++ 1/Program.cs
@@ -3,31 +3,37 @@
 //[345, 897, 568, 234] -> 2
 
 Random random = new Random();
-int n =random.Next(1,21);
+int n = random.Next(1, 21);
 
-int [] array = new int[n];
+int[] array = new int[n];
 fillArray(array);
 printArray(array);
-Console.WriteLine (" -> "+findNumbers(array));
+Console.WriteLine(" -> " + findNumbers(array));
 
-void fillArray (int [] array){
-for(int i=0;i<array.Length;i++){
-   array[i]=random.Next(100,1000);
-}
+void fillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(100, 1000);
+    }
 }
 
-void printArray (int [] array){
+void printArray(int[] array)
+{
     Console.Write("[");
-for(int i=0;i<array.Length-1;i++){
-    Console.Write(array[i]+", ");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write(array[i] + ", ");
+    }
+    Console.Write(array[array.Length - 1]);
+    Console.Write(" ]");
 }
-Console.Write(array[array.Length-1]);
-Console.Write(" ]");
-}
-int findNumbers(int [] array){
-    int count=0;
-foreach(int i in array){
-    if (i%2==0){count++;}
-}
-return count;
+int findNumbers(int[] array)
+{
+    int count = 0;
+    foreach (int i in array)
+    {
+        if (i % 2 == 0) { count++; }
+    }
+    return count;
 }
