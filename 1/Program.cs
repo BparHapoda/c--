ba1771acf980f70c594773feed33f43fd2aa25ed
Bpar_Hapoda <cddvdrom@yandex.ru>
2022-12-06 02:20:39 +0300

    sss

--- 1/Program.cs
+++ 1/Program.cs
@@ -2,4 +2,26 @@
 // Напишите программу, которая покажет количество чётных чисел в массиве.
 //[345, 897, 568, 234] -> 2
 
-Console.WriteLine("Hello, World1111!");
+Random random = new Random();
+int n =random.Next(1,21);
+int [] array = new int[n];
+fillArray(array);
+printArray(array);
+
+void fillArray (int [] array){
+for(int i=0;i<array.Length;i++){
+    array[i]=random.Next(100,1000);
+
+}
+
+
+}
+
+void printArray (int [] array){
+for(int i=0;i<array.Length;i++){
+    Console.Write(array[i]+" ");
+}
+}
+int findNumbers(int [] array){
+    
+}
