d0a0bde1d8b0e28f869273290f60a78a772b8378
Bpar_Hapoda <cddvdrom@yandex.ru>
2022-12-06 02:33:02 +0300

    last 1

--- 1/Program.cs
+++ 1/Program.cs
@@ -4,24 +4,30 @@
 
 Random random = new Random();
 int n =random.Next(1,21);
+
 int [] array = new int[n];
 fillArray(array);
 printArray(array);
+Console.WriteLine (" -> "+findNumbers(array));
 
 void fillArray (int [] array){
 for(int i=0;i<array.Length;i++){
-    array[i]=random.Next(100,1000);
-
+   array[i]=random.Next(100,1000);
 }
-
-
 }
 
 void printArray (int [] array){
-for(int i=0;i<array.Length;i++){
-    Console.Write(array[i]+" ");
+    Console.Write("[");
+for(int i=0;i<array.Length-1;i++){
+    Console.Write(array[i]+", ");
 }
+Console.Write(array[array.Length-1]);
+Console.Write(" ]");
 }
 int findNumbers(int [] array){
-    
+    int count=0;
+foreach(int i in array){
+    if (i%2==0){count++;}
+}
+return count;
 }
